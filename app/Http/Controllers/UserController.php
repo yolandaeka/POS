376f46aedf120448e4fa4e4e60e6be1a872ef57e
Your Name <you@example.com>
2024-10-03 23:47:15 +0700

    JS 6 - Praktikum 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -215,27 +215,28 @@ class UserController extends Controller
     public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
-            ->with('level');
+                    ->with('level');
 
-        // filtwe data user berdasarkan level_id
-        if($request->level_id){
+        //Filter data user berdasarkan level_id
+        if ($request->level_id) {
             $users->where('level_id', $request->level_id);
         }
 
         return DataTables::of($users)
-            // Menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
+            // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addIndexColumn()
-            // Menambahkan kolom aksi (tombol Detail, Edit, Hapus)
-            ->addColumn('aksi', function ($user) {
-                $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> ';
-                $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
-                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/user/' . $user->user_id) . '">'
+            ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
+                /*$btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="'.url('/user/'.$user->user_id).'">'
                     . csrf_field() . method_field('DELETE') .
-                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';*/
+                    $btn = '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/show_ajax').'\')" class="btn btn-info btn-sm">Detail</button> ';
+                    $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/edit_ajax').'\')" class="btn btn-warning btn-sm">Edit</button> ';
+                    $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/delete_ajax').'\')" class="btn btn-danger btn-sm">Hapus</button> ';
                 return $btn;
             })
-            // Memberitahu bahwa kolom aksi berisi HTML
-            ->rawColumns(['aksi'])
+            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
             ->make(true);
     }
 
@@ -411,6 +412,57 @@ class UserController extends Controller
         }
 
         redirect('/');
+    }
+
+    public function edit_ajax(string $id){
+
+        $user = UserModel::find($id);
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.edit_ajax', ['user'=>$user, 'level' => $level]);
+    }
+
+    //Menyimpan perubahan data user AJAX
+    public function update_ajax(Request $request, $id){
+        //periksa bila request dari ajax atau bukan
+        if($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|max:20|unique:m_user,username,'.$id.',user_id',
+                'nama' => 'required|max:100',
+                'password' => 'nullable|min:6|max:20'
+            ];
+
+            // use Illuminate\Support\Facades\Validator
+            $validator = Validator::make($request->all(), $rules);
+            
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false, // respon json, true: berhasil, false: gagal
+                    'message' => 'Validasi gagal.',
+                    'msgField' => $validator->errors() // menunjukkan field mana yang error
+                ]);
+            }
+
+            $check = UserModel::find($id);
+            if ($check) {
+                if(!$request->filled('password') ){ // jika password tidak diisi, maka hapus dari request
+                    $request->request->remove('password');
+                }
+
+                $check->update($request->all());
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil diupdate'
+                ]);
+            } else{
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -78,13 +78,16 @@ Route::group(['prefix' => 'user'], function(){
     Route::get('/', [UserController::class, 'index']);  // menampilkan halaman user
     Route::post('/list', [UserController::class, 'list'] );    //menampilkan data user dalam bentuk json datatables
     Route::get('/create', [UserController::class, 'create']);  //menampilkan halaman tambah user
-    Route::post('/', [UserController::class,'store']);      //menyimpan data user baru
-    // Route::get('/{id}', [UserController::class, 'show']);       //menampilkan detai user
-    Route::get('/{id}/edit', [UserController::class, 'edit']);        //menampilkan halaman form user edit
-    Route::put('/{id}', [UserController::class, 'update']);         //menyimpan perubahan data user
-    Route::delete('/{id}', [UserController::class, 'destroy']);     //mengahpus data user
     Route::get('/create_ajax', [UserController::class, 'create_ajax']); //Menampilkan halaman form tambah user Ajax
     Route::post('/ajax', [UserController::class, 'store_ajax']); // Menyimpan data user baru Ajax 
+    Route::post('/', [UserController::class,'store']);      //menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);       //menampilkan detai user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);        //menampilkan halaman form user edit
+    Route::put('/{id}', [UserController::class, 'update']);         //menyimpan perubahan data user
+    Route::get('/{id}/edit_ajax', [UserController::class,'edit_ajax']); //menampilkan halaman form edit user ajax
+    Route::put('/{id}/update_ajax', [UserController::class,'update_ajax']);   //menyimpan halaman form edit user ajax
+    Route::delete('/{id}', [UserController::class, 'destroy']);     //mengahpus data user
+
 });
 
 Route::group(['prefix' => 'level'], function(){
